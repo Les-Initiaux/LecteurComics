405475b15aeeabd5e52b2e782e8fc9dc7af37b0f
Romain Morice <romain.morice@gmail.com>
2014-02-21 15:38:31 +0100

    Gestion évènements

--- test.js
+++ test.js
@@ -10,28 +10,70 @@ var eltLeft = document.getElementById("go_left");
 var eltRight = document.getElementById("go_right");
 
 var content = document.getElementById("content");
-
-// eltPages.style.display = "none";
-
-//lecture fichier rar
+var i = 0;
+var maxElement = 0;
 var tab=new Array;
+eltPages.style.display = "none";
+
+//lecture fichier rar et zip
 document.querySelector('input[type="file"]').onchange = function(e) {
+	i=0;
+	t = filePicker.value.lastIndexOf("/");
+	console.log("local name = "+t);
+	eltPages.style.display="block";
+	// lecture fichier rar
 	var file = RarArchive(this.files[0], function(err) {
 		if(err) {
 			console.log(err);
 			return;
 		}
-		var i = 0;
 		this.entries.forEach(function(val) {
 		console.log(val.path);
 		tab[i]=val.path;
 		console.log(i+" :"+tab[i]);
 		i=i+1;
-		});
+		});	
+		maxElement = i-1;
+		i=0;
 	});
-
-	var image = "<img src=" + tab[0] + " alt=\"page\" width=\"250\" height=\"400\"/>"; 
+	
+	
+	// lecture fichier zip
+	
+	
+	
+	
+	console.log(i);
+	console.log(maxElement);
+	var image = "<img src=" + tab[i] + " alt=\"page\" width=\"250\" height=\"400\"/>"; 
 	console.log(image);
 	eltFlip.innerHTML=image;
-	console.log("passé!");
+};
+
+eltLeft.onclick = function(){
+	if(i!=0){
+		i=i-1;
+	}
+	else {
+		i=0;
+	}
+	console.log(maxElement);
+	console.log(i);
+	console.log(tab[i]);
+	var image = "<img src=" + tab[i] + " alt=\"page\" width=\"250\" height=\"400\"/>"; 
+	eltFlip.innerHTML=image;
+};
+
+eltRight.onclick = function(){
+	if(i<maxElement){
+		i=i+1;
+	}
+	else {
+		i=0;
+	}
+	console.log(maxElement);
+	console.log(i);
+	console.log(tab[i]);
+	var image = "<img src=" + tab[i] + " alt=\"page\" width=\"250\" height=\"400\"/>"; 
+	eltFlip.innerHTML=image;
 };
